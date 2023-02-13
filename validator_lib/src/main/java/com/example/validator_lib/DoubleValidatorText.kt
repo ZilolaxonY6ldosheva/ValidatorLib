bc921bf5d00ea116c142022f01b43b2883a332d3
zilolaxon <zilolakhon2007@gmail.com>
2023-02-13 17:34:35 +0500

    start commit

--- validator_lib/src/main/java/com/example/validator_lib/DoubleValidatorText.kt
+++ validator_lib/src/main/java/com/example/validator_lib/DoubleValidatorText.kt
@@ -3,6 +3,7 @@ package com.example.validator_lib
 import android.content.Context
 import android.content.res.TypedArray
 import android.text.Editable
+import android.text.InputType
 import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.LayoutInflater
@@ -12,76 +13,83 @@ import androidx.appcompat.widget.AppCompatEditText
 import kotlinx.android.synthetic.main.double_edit_txt.view.*
 
 class DoubleValidatorText(context: Context, attrs: AttributeSet?) :
-    LinearLayout(context,attrs) {
+    LinearLayout(context, attrs) {
 
 
-//    var text: String? = null
+    var atributes: TypedArray =
+        context.obtainStyledAttributes(attrs, R.styleable.ValidatorEditText)
+    val format: Int = atributes.getInt(R.styleable.ValidatorEditText_format, 0)
 
     init {
-         var atributes: TypedArray =
-            context.obtainStyledAttributes(attrs, R.styleable.ValidatorEditText)
-        LayoutInflater.from(context).inflate( R.layout.double_edit_txt, this,true)
+
+        LayoutInflater.from(context).inflate(R.layout.double_edit_txt, this, true)
         orientation = HORIZONTAL
-//        text = atributes.getString(R.styleable.ValidatorEditText_text1)
+
         atributes.recycle()
     }
 
-    fun setError(switch: Int) {
-        when (switch) {
-            2 -> {
+
+        fun setError(switch: Int) {
+            when (switch) {
+                2 -> {
+                    re_password_img.setImageResource(R.drawable.ic_round_error_24)
+                    password_img.setImageResource(R.drawable.ic_round_error_24)
+                }
+                1 -> {
+                    re_password_img.setImageResource(R.drawable.ic_round_error_24)
+
+                }
+                0 -> {
+                    password_img.setImageResource(R.drawable.ic_round_error_24)
+
+                }
+            }
+
+
+        }
+
+
+        fun isNotEmpty(): Boolean {
+            if (re_password.text.isEmpty() && password.text.isEmpty()) {
+                setError(2)
+            } else if (re_password.text.isEmpty() && password.text.isNotEmpty()) {
+                setError(1)
+            } else if (re_password.text.isNotEmpty() && password.text.isEmpty()) {
+                setError(0)
+            }
+            return re_password.text!!.isNotEmpty() && password.text!!.isNotEmpty()
+        }
+
+
+        fun setTextListeners() {
+            re_password.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    checkBothAndSetDrawable(s == password.text)
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+
+                }
+
+            })
+        }
+
+        fun checkBothAndSetDrawable(same: Boolean) {
+            if (same) {
+                re_password_img.setImageResource(R.drawable.ic_round_check_circle_24)
+
+            } else {
                 re_password_img.setImageResource(R.drawable.ic_round_error_24)
-                password_img.setImageResource(R.drawable.ic_round_error_24)
-            }
-            1 -> {
-                re_password_img.setImageResource(R.drawable.ic_round_error_24)
-
-            }
-            0 -> {
-                password_img.setImageResource(R.drawable.ic_round_error_24)
 
             }
         }
-
-
     }
-
-
-    fun isNotEmpty(): Boolean {
-        if (re_password.text.isEmpty() && password.text.isEmpty()) {
-            setError(2)
-        } else if (re_password.text.isEmpty() && password.text.isNotEmpty()) {
-            setError(1)
-        } else if (re_password.text.isNotEmpty() && password.text.isEmpty()) {
-            setError(0)
-        }
-        return re_password.text!!.isNotEmpty() && password.text!!.isNotEmpty()
-    }
-
-
-    fun setTextListeners() {
-        re_password.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                checkBothAndSetDrawable(s == password.text)
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-
-            }
-
-        })
-    }
-
-    fun checkBothAndSetDrawable(same: Boolean) {
-        if (same) {
-            re_password_img.setImageResource(R.drawable.ic_round_check_circle_24)
-
-        } else {
-            re_password_img.setImageResource(R.drawable.ic_round_error_24)
-
-        }
-    }
-}
